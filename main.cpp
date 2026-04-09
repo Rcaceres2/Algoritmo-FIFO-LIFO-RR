fb735d71589cf986688daacf12e121b337b9bce8
Rcaceres2 <reynaldocaceres0@gmail.com>
2026-04-08 23:00:04 -0400

    Creacion de archivo main
    Main y comprobacion de existencia de data.txt

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    Proceso base[MAX_PROCESOS]; 
+    int n = 0;                  
+    
+    ifstream entrada("data.txt");
+    if (!entrada.is_open())
+    {
+        cout << "No se pudo abrir data.txt" << endl;
+        return 1;
+    }
+
+    f_out.close();
+    return 0;
+}
